1ea22cdf80cde2c7276f57a13182c4799ac4520d
Damien <damiensabo@live.fr>
2023-07-01 15:23:34 +0200

    Create Express.js server with /phones & /phones/:id GET routes

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const phones = require('../data/phones.json');
+
+app.get('/phones', (req, res) => {
+  res.json(phones);
+});
+
+app.get('/phones/:id', (req, res) => {
+  const phoneId = parseInt(req.params.id);
+  const phone = phones.find(phone => phone.id === phoneId);
+  if (phone) {
+    res.json(phone);
+  } else {
+    res.status(404).json({ error: 'Phone not found' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
