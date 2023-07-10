7917d71cabdbba35075d3d13ae31e74cee66f24d
Damien <damiensabo@live.fr>
2023-07-11 00:15:45 +0200

    Fix fetchPhones() in PhoneList.js

--- frontend/src/components/PhoneList/PhoneList.js
+++ frontend/src/components/PhoneList/PhoneList.js
@@ -12,7 +12,7 @@ const PhoneList = ({ onSelectPhone }) => {
     const fetchPhones = async () => {
       try {
         const response = await getPhones();
-        setPhones(response.data);
+        setPhones(response);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching phones:', error);
